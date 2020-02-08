bbc083974f1492710564f91b5dc5ef57d49d836f
Avtar Singh <avi.avtargill@gmail.com>
2020-02-08 13:46:42 -0500

    Initial Commit

---
+++ app/src/main/java/com/example/bookbuy/UserHomePage.java
@@ -0,0 +1,14 @@
+package com.example.bookbuy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserHomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_home_page);
+    }
+}
