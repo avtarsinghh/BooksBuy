951d9f9b8df8fadbb4fdaca645cf505ef79db747
Amardeep kaur <annukaur25101992@gmail.com>
2020-02-08 15:51:20 -0500

    UserHomePage designed with cardview

---
+++ app/src/main/java/com/example/bookbuy/Books.java
@@ -0,0 +1,18 @@
+package com.example.bookbuy;
+
+public class Books
+{
+    String id,title,author,publisher,language,edition,rating,yop,description;
+
+    public Books(String id, String title, String author, String publisher, String language, String edition, String rating, String yop, String description) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.publisher = publisher;
+        this.language = language;
+        this.edition = edition;
+        this.rating = rating;
+        this.yop = yop;
+        this.description = description;
+    }
+}
--- app/src/main/java/com/example/bookbuy/UserHomePage.java
+++ app/src/main/java/com/example/bookbuy/UserHomePage.java
@@ -1,14 +1,50 @@
 package com.example.bookbuy;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class UserHomePage extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_home_page);
+
+        recyclerView=findViewById(R.id.recyclerview);
+
+
+
+        db.collection("books")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult())
+                            {
+                                Toast.makeText(UserHomePage.this,"=>",Toast.LENGTH_SHORT);
+                            }
+                        } else
+                            {
+                                Toast.makeText(UserHomePage.this,"error",Toast.LENGTH_SHORT);
+                        }
+                    }
+                });
     }
 }
