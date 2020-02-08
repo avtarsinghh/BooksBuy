681e712484e3db77673d43e77657f21cc52daa7c
Amardeep kaur <annukaur25101992@gmail.com>
2020-02-08 15:57:22 -0500

    little bit changes in book class

--- app/src/main/java/com/example/bookbuy/Books.java
+++ app/src/main/java/com/example/bookbuy/Books.java
@@ -3,16 +3,85 @@ package com.example.bookbuy;
 public class Books
 {
     String id,title,author,publisher,language,edition,rating,yop,description;
+    String image;
 
-    public Books(String id, String title, String author, String publisher, String language, String edition, String rating, String yop, String description) {
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
         this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
         this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
         this.author = author;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
         this.publisher = publisher;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
         this.language = language;
+    }
+
+    public String getEdition() {
+        return edition;
+    }
+
+    public void setEdition(String edition) {
         this.edition = edition;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
         this.rating = rating;
+    }
+
+    public String getYop() {
+        return yop;
+    }
+
+    public void setYop(String yop) {
         this.yop = yop;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
         this.description = description;
     }
 }
