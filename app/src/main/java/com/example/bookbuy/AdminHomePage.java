9be02e7ccdbeb21c273dc902e474ec1ad60ff34b
Avtar Singh <avi.avtargill@gmail.com>
2020-02-08 16:00:55 -0500

    Recycler view added to Admin home page

--- app/src/main/java/com/example/bookbuy/AdminHomePage.java
+++ app/src/main/java/com/example/bookbuy/AdminHomePage.java
@@ -1,14 +1,20 @@
 package com.example.bookbuy;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
-public class AdminHomePage extends AppCompatActivity {
+import com.google.firebase.firestore.FirebaseFirestore;
 
+public class AdminHomePage extends AppCompatActivity {
+    RecyclerView recyclerView;
+    FirebaseFirestore firestore;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_home_page);
+        recyclerView = findViewById(R.id.recyclerView);
+        firestore = FirebaseFirestore.getInstance();
     }
 }
