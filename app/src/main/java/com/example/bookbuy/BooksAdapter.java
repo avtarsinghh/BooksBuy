56f02116bfcaadf7f061ddf3bf3b9cca9dfec8ff
Amardeep Grewal <annukaur25101992@gmail.com>
2020-02-19 17:14:19 -0500

    UserHomePage

--- app/src/main/java/com/example/bookbuy/BooksAdapter.java
+++ app/src/main/java/com/example/bookbuy/BooksAdapter.java
@@ -1,42 +1,64 @@
 package com.example.bookbuy;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class BooksAdapter
-{
+import com.squareup.picasso.Picasso;
 
-  class BooksViewHolder extends RecyclerView.ViewHolder
-  {
+import java.util.ArrayList;
 
+public class BooksAdapter extends RecyclerView.Adapter<BooksAdapter.ViewHolder> {
+    LayoutInflater layoutInflater;
+    Context context;
+    ArrayList<Books> books;
 
-      CardView userCv;
-      TextView txtTitle;
-      TextView txtAuthor;
-      TextView txtPublisher;
-      TextView txtEdition;
-      ImageView imgbook;
-      Button btnBuy;
+    public BooksAdapter(Context context, ArrayList<Books> books) {
+        layoutInflater = LayoutInflater.from(context);
+        this.context = context;
+        this.books = books;
+    }
 
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = layoutInflater.inflate(R.layout.userhomepagecardview, parent, false);
+        return new ViewHolder(view);
+    }
 
-      public BooksViewHolder(@NonNull View itemView)
-      {
-          super( itemView );
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int position) {
+        viewHolder.titleB.setText(books.get(position).getTitle());
+        viewHolder.authorB.setText(books.get(position).getAuthor());
+        viewHolder.publisherB.setText(books.get(position).getPublisher());
+        viewHolder.languageB.setText(books.get(position).getLanguage());
+        viewHolder.editionB.setText(books.get(position).getEdition());
+        Picasso.get().load(books.get(position).getImage()).into(viewHolder.imgBook);
+    }
 
-          userCv=(CardView)itemView.findViewById( R.id.usercv );
-          txtTitle = (TextView)itemView.findViewById(R.id.txtbooktitle);
-          txtAuthor = (TextView)itemView.findViewById(R.id.txtbookauthor);
+    @Override
+    public int getItemCount() {
+        return books.size();
+    }
 
-          imgbook = (ImageView)itemView.findViewById(R.id.bookimage);
-
-      }
-  }
-
-
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView imgBook;
+        TextView titleB, authorB, publisherB, languageB, editionB, ratingB, yopB;
+        Button modifyBtn;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imgBook = itemView.findViewById(R.id.bookimage);
+            titleB = itemView.findViewById(R.id.txtbooktitle);
+            authorB = itemView.findViewById(R.id.txtbookauthor);
+            publisherB = itemView.findViewById(R.id.txtbookpublisher);
+            editionB = itemView.findViewById(R.id.txtbookedition);
+        }
+    }
 }
--- app/src/main/java/com/example/bookbuy/UserHomePage.java
+++ app/src/main/java/com/example/bookbuy/UserHomePage.java
@@ -2,6 +2,7 @@ package com.example.bookbuy;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
@@ -14,37 +15,44 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.ArrayList;
+
 public class UserHomePage extends AppCompatActivity {
 
     RecyclerView recyclerView;
-
-
-    FirebaseFirestore db = FirebaseFirestore.getInstance();
-
+    FirebaseFirestore firestore;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user_home_page);
+        setContentView(R.layout.activity_admin_home_page);
+        recyclerView = findViewById(R.id.recyclerView);
+        firestore = FirebaseFirestore.getInstance();
 
-        recyclerView=findViewById(R.id.recyclerview);
-
-
-
-        db.collection("books")
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if (task.isSuccessful()) {
-                            for (QueryDocumentSnapshot document : task.getResult())
-                            {
-                                Toast.makeText(UserHomePage.this,"=>",Toast.LENGTH_SHORT);
-                            }
-                        } else
-                            {
-                                Toast.makeText(UserHomePage.this,"error",Toast.LENGTH_SHORT);
-                        }
-                    }
-                });
+        firestore.collection("books").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                ArrayList<Books> books = new ArrayList<>();
+                for(QueryDocumentSnapshot snapshot : task.getResult()){
+                    Books book = new Books();
+                    book.setId(snapshot.getId());
+                    book.setTitle(""+snapshot.getData().get("title"));
+                    book.setPublisher(""+snapshot.getData().get("publisher"));
+                    book.setLanguage(""+snapshot.getData().get("language"));
+                    book.setEdition(""+snapshot.getData().get("edition"));
+                    book.setRating(""+snapshot.getData().get("rating"));
+                    book.setYop(""+snapshot.getData().get("yop"));
+                    book.setDescription(""+snapshot.getData().get("description"));
+                    book.setAuthor(""+snapshot.getData().get("author"));
+                    book.setImage(""+snapshot.getData().get("linkToImage"));
+                    books.add(book);
+                }
+                BooksAdapter booksAdapter = new BooksAdapter(
+                        getApplicationContext(), books
+                );
+                recyclerView.setAdapter(booksAdapter);
+                RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+                recyclerView.setLayoutManager(layoutManager);
+            }
+        });
     }
 }
