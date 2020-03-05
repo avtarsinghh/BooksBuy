ec4c5e1b85be4969d5dce0305cf238375a17170e
Amardeep Grewal <annukaur25101992@gmail.com>
2020-03-05 18:15:54 -0500

    minor changes for logout ...

--- app/src/main/java/com/example/bookbuy/UserHomePage.java
+++ app/src/main/java/com/example/bookbuy/UserHomePage.java
@@ -2,16 +2,23 @@ package com.example.bookbuy;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toolbar;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
@@ -22,7 +29,8 @@ public class UserHomePage extends AppCompatActivity {
 
     RecyclerView recyclerViewUh;
     FirebaseFirestore firestoreuh;
-    Button btnlogout;
+    Toolbar toolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -33,7 +41,8 @@ public class UserHomePage extends AppCompatActivity {
         recyclerViewUh = findViewById(R.id.recyclerview );
         firestoreuh = FirebaseFirestore.getInstance();
 
-       btnlogout=findViewById(R.id.btnLogout);
+        toolbar =findViewById( R.id.menuToolbar );
+        setSupportActionBar( toolbar );
 
         firestoreuh.collection("books").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
@@ -62,13 +71,31 @@ public class UserHomePage extends AppCompatActivity {
             }
         });
 
-        btnlogout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                Intent intent=new Intent(UserHomePage.this,Login.class);
-                startActivity(intent);
-            }
-        });
   }
+
+    private void setSupportActionBar(Toolbar toolbar) {
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater=getMenuInflater();
+        menuInflater.inflate( R.menu.menu,menu );
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId())
+        {
+            case R.id.menuLogout:
+                FirebaseAuth.getInstance().signOut();
+                finish();
+                Intent intent = new Intent(UserHomePage.this, Login.class);
+                startActivity(intent);
+        }
+
+        return true;
+    }
 }
