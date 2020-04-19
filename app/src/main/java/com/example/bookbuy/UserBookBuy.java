5b5e2c26465ee224a14096e55f1d161eba932b26
Amardeep Grewal <annukaur25101992@gmail.com>
2020-04-18 20:46:07 -0400

    Final commit. UserHomepage, sending data from one Activity to another activity done.

--- app/src/main/java/com/example/bookbuy/UserBookBuy.java
+++ app/src/main/java/com/example/bookbuy/UserBookBuy.java
@@ -1,16 +1,24 @@
 package com.example.bookbuy;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
 
 public class UserBookBuy extends AppCompatActivity {
-    EditText edId, edTitle, edAuthor, edPublisher, edLanguage, edEdition, edRating, edYOP, eddescription;
+    TextView  edTitle, edAuthor, edPublisher, edLanguage, edEdition, edRating, edYOP, eddescription;
+    ImageView imgBookBB;
     Button btnPurchase;
 
     Intent intent;
@@ -22,29 +30,56 @@ public class UserBookBuy extends AppCompatActivity {
 
         intent=getIntent();
 
-        edId = (EditText)findViewById(R.id.edt1);
-        edTitle = (EditText)findViewById(R.id.edt2);
-        edAuthor = (EditText)findViewById(R.id.edt3);
-        edPublisher = (EditText)findViewById(R.id.edt4);
-        edLanguage = (EditText)findViewById(R.id.edt5);
-        edEdition = (EditText)findViewById(R.id.edt6);
-        edRating = (EditText)findViewById(R.id.edt7);
-        edYOP = (EditText)findViewById(R.id.edt8);
-        eddescription = (EditText)findViewById(R.id.edt9);
+        edTitle = (TextView) findViewById(R.id.edtTitleBB);
+        edAuthor = (TextView) findViewById(R.id.edtAuthorBB);
+        edPublisher = (TextView) findViewById(R.id.edtPublisherBB);
+        edLanguage = (TextView) findViewById(R.id.edtLanguageBB);
+        edEdition = (TextView) findViewById(R.id.edtEditionBB);
+        edRating = (TextView) findViewById(R.id.edtRatingBB);
+        edYOP = (TextView) findViewById(R.id.edtYOPbb);
+        eddescription = (TextView) findViewById(R.id.edtDescriptionBB);
+        imgBookBB=(ImageView)findViewById( R.id.imgImageBB );
+
         btnPurchase = (Button)findViewById(R.id.btnClick);
 
-        String eTitle = getIntent().getStringExtra("title");
+      /*  String strTitle = getIntent().getStringExtra("title");
+        String strAuthor = getIntent().getStringExtra("author");
+        String strPublisher = getIntent().getStringExtra("publisher");
+        String strLang = getIntent().getStringExtra("language");
+        String strEdition = getIntent().getStringExtra("edition");
+        String strRating = getIntent().getStringExtra("rating");
+        String strYOP = getIntent().getStringExtra("yop");
+        String strDescription = getIntent().getStringExtra("description");
+        String strImage = getIntent().getStringExtra("linkToImage");
 
-        edTitle.setText( eTitle );
 
-        btnPurchase.setOnClickListener(new View.OnClickListener() {
+        edTitle.setText( strTitle );
+        edAuthor.setText( strAuthor );
+        edPublisher.setText( strPublisher );
+        edLanguage.setText( strLang );
+        edEdition.setText( strEdition );
+        edRating.setText( strRating );
+        edYOP.setText( strYOP );
+        eddescription.setText( strDescription );
+
+        Picasso.get().load(strImage).into(imgBookBB);
+
+        btnPurchase.setOnClickListener( new View.OnClickListener() {
             @Override
-            public void onClick(View v)
-            {
-
+            public void onClick(View v) {
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder( UserBookBuy.this );
+                alertDialogBuilder.setTitle( "Information" )
+                        .setMessage( "Book is resreved. You can buy it from shop" )
+                        .setPositiveButton( "Ok",new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog,int which) {
+                                Intent intent=new Intent( UserBookBuy.this,UserHomePage.class );
+                                startActivity( intent );
+                            }
+                        } );
+                alertDialogBuilder.show();
             }
-        });
-
+        } );   */
 
     }
 }
--- app/src/main/java/com/example/bookbuy/UserHomePage.java
+++ app/src/main/java/com/example/bookbuy/UserHomePage.java
@@ -8,9 +8,11 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toolbar;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -22,12 +24,18 @@ import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+
 public class UserHomePage extends AppCompatActivity {
 
     RecyclerView recyclerViewUh;
     FirebaseFirestore firestoreuh;
-    Toolbar toolbar;
+   // Toolbar toolbar;
 
+    Retrofit retrofit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -38,10 +46,35 @@ public class UserHomePage extends AppCompatActivity {
         recyclerViewUh = findViewById(R.id.recyclerview );
         firestoreuh = FirebaseFirestore.getInstance();
 
-        toolbar =findViewById( R.id.menuToolbar );
-        setSupportActionBar( toolbar );
+       // toolbar =findViewById( R.id.menuToolbar );
+      //  setSupportActionBar( toolbar );
 
-        firestoreuh.collection("books").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+        retrofit = RetrofitInstance.getRetrofitInstance();
+
+        DataService dataService = retrofit.create(DataService.class);
+        Call<ArrayList<Book>> call = dataService.getBooks();
+        call.enqueue(new Callback<ArrayList<Book>>() {
+            @Override
+            public void onResponse(Call<ArrayList<Book>> call, Response<ArrayList<Book>> response) {
+                ArrayList<Book>  books = new ArrayList<>();
+                Log.i("body", ""+response.body());
+                for(Book book : response.body()){
+                    books.add(book);
+                }
+                BooksAdapter booksAdapter = new BooksAdapter( UserHomePage.this, books);
+                recyclerViewUh.setAdapter(booksAdapter);
+                RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+                recyclerViewUh.setLayoutManager(layoutManager);
+            }
+
+            @Override
+            public void onFailure(Call<ArrayList<Book>> call, Throwable t)
+            {
+                Log.i("body", t.getMessage());
+            }
+        });
+
+      /*  firestoreuh.collection("books").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
             public void onComplete(@NonNull Task<QuerySnapshot> task) {
                 ArrayList<Book> books = new ArrayList<>();
@@ -65,7 +98,7 @@ public class UserHomePage extends AppCompatActivity {
                 RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
                 recyclerViewUh.setLayoutManager(layoutManager);
             }
-        });
+        });*/
 
   }
 
